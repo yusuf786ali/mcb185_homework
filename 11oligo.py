3ed1e24260dee04ef95c8dd5ce79b74d4b9c1c38
yusuf786ali <yusuf876ali@gmail.com>
2025-01-21 15:56:01 -0800

    update

--- 11oligo.py
+++ 11oligo.py
@@ -1,7 +1,6 @@
-import math
 def tm(a, c, g, t):
     nt = a + c + g + t
     if nt <= 13: print((a + t*2)+ (g + c)*4)
-    else:          print(64.9 + (41*(g + c - 16.4)) / (a + t + g + c))
+    else:        print(64.9 + (41*(g + c - 16.4)) / (a + t + g + c))
 
 print(tm(1, 1, 3, 4))
